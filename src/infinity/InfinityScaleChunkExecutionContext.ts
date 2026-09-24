454003edd7470b79d1575badea295ae3184dd839
Ayka11 <32898816+Ayka11@users.noreply.github.com>
2026-09-24 16:28:25 +0400

    Use canonical source-side boundary cell predicate

--- src/infinity/InfinityScaleChunkExecutionContext.ts
+++ src/infinity/InfinityScaleChunkExecutionContext.ts
@@ -1,5 +1,5 @@
 import type { InfinityScaleExecutionPlan } from "./InfinityScaleExecutionAdapter";
-import { validateInfinityScaleBoundaryGeometry } from "./InfinityScaleLODBoundaryGeometry";
+import { isInfinityScaleBoundaryCell } from "./InfinityScaleLODBoundaryCellMapper";
 
 export interface ExecutionCellRange {
   minX: number;
@@ -204,7 +204,7 @@ export class InfinityScaleChunkExecutionContext {
     for (const sourceRelations of this.boundaryRelationsBySource.values()) {
       for (const relation of sourceRelations) {
         const spec = this.relationToTransferSpec(relation);
-        if (validateInfinityScaleBoundaryGeometry(spec, [x, y, z], this.chunkSize)) {
+        if (isInfinityScaleBoundaryCell(spec, [x, y, z], this.chunkSize)) {
           relations.push({ ...relation });
         }
       }
