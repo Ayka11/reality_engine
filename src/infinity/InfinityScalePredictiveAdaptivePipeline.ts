b83cdcf4cbb5d7b20058235fd043caf0c960dee1
Ayka11 <32898816+Ayka11@users.noreply.github.com>
2026-09-24 20:07:21 +0400

    Integrate GPU completion proof into adaptive pipeline

--- src/infinity/InfinityScalePredictiveAdaptivePipeline.ts
+++ src/infinity/InfinityScalePredictiveAdaptivePipeline.ts
@@ -1,6 +1,7 @@
 import { InfinityScaleAdaptiveTransferCompiler, InfinityScaleAdaptiveMutation } from "./InfinityScaleAdaptiveTransferCompiler";
 import { InfinityScaleAdaptiveExecutionGraphCompiler } from "./InfinityScaleAdaptiveExecutionGraphCompiler";
 import { InfinityScaleGPUExecutionPlanAdapter, InfinityScaleGPUExecutionBudget } from "./InfinityScaleGPUExecutionPlanAdapter";
+import { validateInfinityScaleAdaptiveGPUCompletion, type InfinityScaleAdaptiveGPUCompletion } from "./InfinityScaleAdaptiveGPUCompletion";
 import { validateInfinityScaleAdaptiveTransactionGate, InfinityScaleAdaptiveTransactionGateResult } from "./InfinityScaleAdaptiveTransactionGate";
 
 export interface InfinityScalePredictiveAdaptivePipelineInput {
@@ -12,6 +13,7 @@ export interface InfinityScalePredictiveAdaptivePipelineInput {
   gpuBudget: InfinityScaleGPUExecutionBudget;
   conservationValid: boolean;
   gpuComplete: boolean;
+  completedGPUDispatchBatchIds?: string[];
 }
 
 export interface InfinityScalePredictiveAdaptivePipelineResult {
@@ -21,6 +23,7 @@ export interface InfinityScalePredictiveAdaptivePipelineResult {
   mutationCount: number;
   transferCount: number;
   deferredNodeIds: string[];
+  gpuCompletion?: InfinityScaleAdaptiveGPUCompletion;
   transaction: InfinityScaleAdaptiveTransactionGateResult;
   executable: boolean;
   commitReady: boolean;
@@ -36,49 +39,37 @@ export class InfinityScalePredictiveAdaptivePipeline {
     const transferPlan = this.transferCompiler.compile(input.mutations);
     const graph = this.graphCompiler.compile(transferPlan);
     const gpuPlan = this.gpuAdapter.compile(graph, input.gpuBudget);
-
+    const gpuCompletion = input.completedGPUDispatchBatchIds
+      ? validateInfinityScaleAdaptiveGPUCompletion(graph, gpuPlan, input.completedGPUDispatchBatchIds)
+      : undefined;
     const transaction = validateInfinityScaleAdaptiveTransactionGate({
       sourceStateRevision: input.stateRevision,
       expectedStateRevision: input.expectedStateRevision,
       sourceTopologyRevision: input.topologyRevision,
       expectedTopologyRevision: input.expectedTopologyRevision,
-      graph,
-      gpuPlan,
+      graph, gpuPlan,
       conservationValid: input.conservationValid,
       gpuComplete: input.gpuComplete,
+      gpuCompletion,
     });
-
-    const pipelineHash = stableHash(
-      JSON.stringify({
-        transferPlanHash: transferPlan.transferPlanHash,
-        graphHash: graph.graphHash,
-        gpuPlanHash: gpuPlan.planHash,
-        transaction,
-      }),
-    );
-
+    const pipelineHash = stableHash(JSON.stringify({
+      transferPlanHash: transferPlan.transferPlanHash, graphHash: graph.graphHash,
+      gpuPlanHash: gpuPlan.planHash, gpuCompletion, transaction,
+    }));
     return {
       transferPlanHash: transferPlan.transferPlanHash,
       graphHash: graph.graphHash,
       gpuPlanHash: gpuPlan.planHash,
-      mutationCount: transferPlan.mutations.filter(
-        mutation => mutation.fromLOD !== mutation.toLOD,
-      ).length,
+      mutationCount: transferPlan.mutations.filter(m => m.fromLOD !== m.toLOD).length,
       transferCount: transferPlan.transfers.length,
       deferredNodeIds: gpuPlan.deferredNodeIds,
-      transaction,
-      executable: transaction.executable,
-      commitReady: transaction.commitReady,
-      pipelineHash,
+      gpuCompletion, transaction,
+      executable: transaction.executable, commitReady: transaction.commitReady, pipelineHash,
     };
   }
 }
-
 function stableHash(value: string): string {
   let hash = 2166136261;
-  for (let i = 0; i < value.length; i++) {
-    hash ^= value.charCodeAt(i);
-    hash = Math.imul(hash, 16777619);
-  }
+  for (let i = 0; i < value.length; i++) { hash ^= value.charCodeAt(i); hash = Math.imul(hash, 16777619); }
   return (hash >>> 0).toString(16).padStart(8, "0");
 }
