c6f5067764dfd4023121120c5cc5d18cf6b209e6
Ayka11 <32898816+Ayka11@users.noreply.github.com>
2026-09-24 19:41:52 +0400

    Register GPU execution plan adapter regression

--- src/infinity/InfinityScaleRegressionSuite.ts
+++ src/infinity/InfinityScaleRegressionSuite.ts
@@ -23,6 +23,7 @@ import { runInfinityScaleSpatialConstraintClosureRegression } from "./InfinitySc
 import { runInfinityScaleTopologyMutationBridgeRegression } from "./InfinityScaleTopologyMutationBridge.regression";
 import { runInfinityScaleAdaptiveTransferCompilerRegression } from "./InfinityScaleAdaptiveTransferCompiler.regression";
 import { runInfinityScaleAdaptiveExecutionGraphCompilerRegression } from "./InfinityScaleAdaptiveExecutionGraphCompiler.regression";
+import { runInfinityScaleGPUExecutionPlanAdapterRegression } from "./InfinityScaleGPUExecutionPlanAdapter.regression";
 
 export interface InfinityScaleRegressionResult {
   name: string;
@@ -54,6 +55,7 @@ export function runInfinityScaleRegressionSuite(): InfinityScaleRegressionResult
     ["topology-mutation-bridge", runInfinityScaleTopologyMutationBridgeRegression],
     ["adaptive-transfer-compiler", runInfinityScaleAdaptiveTransferCompilerRegression],
     ["adaptive-execution-graph-compiler", runInfinityScaleAdaptiveExecutionGraphCompilerRegression],
+    ["gpu-execution-plan-adapter", runInfinityScaleGPUExecutionPlanAdapterRegression],
   ];
 
   const results: InfinityScaleRegressionResult[] = [];
