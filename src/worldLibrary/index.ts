df68f60090f85601454d4089325cb3fd852096ef
Ayka11 <32898816+Ayka11@users.noreply.github.com>
2026-09-26 22:13:08 +0400

    FEAT: export World Library generation planner

--- src/worldLibrary/index.ts
+++ src/worldLibrary/index.ts
@@ -28,3 +28,4 @@ export * from './SettlementGrowthModel';
 export * from './CivilizationRuntime';
 
 export * from './ProductionInfrastructureRuntime';
+export * from './WorldLibraryGenerationPlanner';
