4b08b7b2c44784da036b6e03965fb7faf94614c1
Ayka11 <32898816+Ayka11@users.noreply.github.com>
2026-09-26 21:45:43 +0400

    FEAT: initialize World Rule Graph registry

--- src/worldLibrary/registry.ts
+++ src/worldLibrary/registry.ts
@@ -2,3 +2,7 @@ import { WORLD_LIBRARY_CATALOG } from './catalog';
 import { WorldLibrary } from './WorldLibrary';
 
 export const worldLibrary = new WorldLibrary(WORLD_LIBRARY_CATALOG);
+import { WORLD_RULES } from './rules';
+import { WorldRuleGraph } from './WorldRuleGraph';
+
+export const worldRuleGraph = new WorldRuleGraph(WORLD_RULES);
